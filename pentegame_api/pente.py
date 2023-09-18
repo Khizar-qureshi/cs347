b2d0cec2873c093d6c0bb9e04f305232a6478749
Khizar Qureshi <qureshik@carleton.edu>
2023-09-18 16:20:11 -0500

    renamed file, updated pente.py

--- pentegame_api/pente.py
+++ pentegame_api/pente.py
@@ -1,7 +1,9 @@
 '''
 Khizar Qureshi & Ntense Obono
-app.py
+Professor Matt Lepenski
+pente.py
 Sep 18 2023
+API for two-player game of Pente
 CS 347 - Advance Software Design 
 '''
 
@@ -15,57 +17,59 @@ games = {}
 current = 0
 
 '''Returns a string representation of the current board'''
-def getBoard(board):
+def get_board(board):
     formatted_board = ""
     for row in board:
         for spot in row:
             formatted_board += spot 
     return formatted_board    
 
-def isCapturable(board, row, col): 
-    char = board[row][col]
-    if col < 19 and board[row][col+1] == char:
+'''Determines if the player can be capturable'''
+def is_capturable(board, row, col, char): 
+    if col < 19 and board[row][col+1] == char and board[row][col]==char:
         return True
-    elif row < 19 and board[row+1][col] == char:
+    elif row < 19 and board[row+1][col] == char and board[row][col]==char:
         return True
-    elif row < 19 and col < 19 and board[row+1][col+1] == char:
+    elif row < 19 and col < 19 and board[row+1][col+1] == char and board[row][col]==char:
         return True
-    elif col > 0 and board[row][col-1] == char:
+    elif col > 0 and board[row][col-1] == char and board[row][col]==char:
         return True
-    elif row > 0 and board[row-1][col] == char:
+    elif row > 0 and board[row-1][col] == char and board[row][col]==char:
         return True
-    elif row >0 and col > 0 and board[row-1][col-1] == char:
+    elif row >0 and col > 0 and board[row-1][col-1] == char and board[row][col]==char:
         return True
     return False
-    
+
+
+
+'''Updates the amount of captures player X has from player O'''
 def updateX(board,row,col):
     nums = 0
-    if isCapturable(board,row,col): 
-        if col < 18 and col > 1 and board[row][col+2] and board[row][col-2] == "O":
+    if is_capturable(board,row,col,'o'): 
+        if col < 18 and col > 1 and board[row][col+2] and board[row][col-2] == "x":
             nums+=1
-        elif row < 18 and row > 1 and board[row+2][col] and board[row-2][col]== "O":
+        elif row < 18 and row > 1 and board[row+2][col] and board[row-2][col]== "x":
             nums+=1
-        elif row < 18 and col < 18 and row > 1 and col > 1 and board[row+2][col+2] and board[row-2][col-2]== "O":
+        elif row < 18 and col < 18 and row > 1 and col > 1 and board[row+2][col+2] and board[row-2][col-2]== "x":
             nums+=1
     return nums
         
+'''Updates the amount of captures player O has from player X'''  
 def updateO(board,row,col):
     nums = 0
-    if isCapturable(board,row,col): 
-        if col < 18 and col > 1 and board[row][col+2] and board[row][col-2] == "X":
+    if is_capturable(board,row,col, 'x'): 
+        if col < 18 and col > 1 and board[row][col+2] and board[row][col-2] == "o":
             nums+=1
-        elif row < 18 and row > 1 and board[row+2][col] and board[row-2][col] == "X":
+        elif row < 18 and row > 1 and board[row+2][col] and board[row-2][col] == "o":
             nums+=1
-        elif row < 18 and col < 18 and row > 1 and col > 1 and board[row+2][col+2] and board[row-2][col-2] == "X":
+        elif row < 18 and col < 18 and row > 1 and col > 1 and board[row+2][col+2] and board[row-2][col-2] == "o":
             nums+=1
     return nums
-        
-
         
 
 @app.route('/')
-def hello():
-    return 'Welcome to a simple API Flask implementation of Pente'
+def main():
+    return 'Welcome to a simple 2 Player API implementation of Pente created by Khizar Qureshi & Ntense Obono'
 
 
 @app.route('/newgame/<player>')
@@ -73,15 +77,13 @@ def new_game(player):
     global current 
     current += 1
     gameID = current
-    #boardSetup = "-" * 361
-    boardSetup = [['-' for i in range(19)] for j in range(19)]
-    if player == 'X' or player == 'x':
-        boardSetup[19//2][19//2] = 'X'
-    formatted_board = getBoard(boardSetup)
+    boardSetup = [['-' for i in range(20)] for j in range(20)]
+    if player == 'x':
+        boardSetup[19//2][19//2] = 'x'
+    formatted_board = get_board(boardSetup)
     gameState = 'player:' + player + '#' + 'board:' + formatted_board + '#' + 'capturedX: 0' + '#' + 'capturedO: 0'
     games[gameID] =  {'player': player, 'board': boardSetup, 'capturedX': 0, 'capturedO': 0}
     return json.dumps({'ID':gameID, 'state':gameState})
-
 
 
 @app.route('/nextmove/<int:gameID>/<int:row>/<int:col>')
@@ -97,63 +99,33 @@ def new_move(gameID, row, col):
     if board[row][col] != '-':
         raise Exception("This spot is already taken!")   
     elif player == 'X' or player == 'x':
-        board[row][col] = 'O'  
-        capturedO = capturedO + updateO(board, row, col) 
+        board[row][col] = 'o'  
+        capturedO = capturedO + updateO(board, row, col) #recalculate capturedO
     else:
-        board[row][col] = 'X' 
-        capturedX = capturedX + updateX(board, row, col)
+        board[row][col] = 'x'
+        capturedX = capturedX + updateX(board, row, col) #recalculate capturedX
 
     # place player's spot 
     temp = False
-    for rowIndex, row in enumerate(board):
+    for rowIndex, row in enumerate(board): 
         for colIndex, col in enumerate(row):
-            if col == "-":
+            if col == "-": # pick the first available spot
                 board[rowIndex][colIndex] = player 
                 myRow, myCol = rowIndex, colIndex
                 temp = True
                 break
         if temp: break
         
-
-    
     #recalculate captureX and captureY based on player's spot
     capturedO = capturedO + updateO(board, myRow, myCol) 
     capturedX = capturedX + updateX(board, myRow, myCol)
-
     #get new string representation of board          
-    formatted_board = getBoard(board)
+    formatted_board = get_board(board)
     # create new game state
     new_game_state = 'player:' + player + '#' + 'board:' + formatted_board + '#' + 'capturedX:' + str(capturedX) + '#' + 'capturedO:' + str(capturedO)
     #update the dictionary for the game_id
     games[gameID] =  {'player': player, 'board': board, 'capturedX': capturedX, 'capturedO': capturedO}
     return json.dumps({'ID':gameID, 'row':myRow, 'column': myCol, 'gameState': new_game_state})
-
-# def isCapturable(board, row, col): 
-#     char = board[row][col]
-#     if col < 19 and board[row][col+1] == char:
-#         return True
-#     elif row < 19 and board[row+1][col] == char:
-#         return True
-#     elif row < 19 and col < 19 and board[row+1][col+1] == char:
-#         return True
-#     elif col > 0 and board[row][col-1] == char:
-#         return True
-#     elif row > 0 and board[row-1][col] == char:
-#         return True
-#     elif row >0 and col > 0 and board[row-1][col-1] == char:
-#         return True
-#     return False
-    
-# def updateX(board,row,col):
-#     nums = 0
-#     if isCapturable(board,row,col): 
-#         if col < 18 and col > 1 and board[row][col+2] and board[row][col-2] == "O":
-#             nums+=1
-#         elif row < 18 and row > 1 and board[row+2][col] and board[row-2][col]== "O":
-#             nums+=1
-#         elif row < 18 and col < 18 and row > 1 and col > 1 and board[row+2][col+2] and board[row-2][col-2]== "O":
-#             nums+=1
-#     return nums
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Simple Flask application")
